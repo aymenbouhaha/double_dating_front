b12f81ae3b267723d8914d8384eb9e7e1c336db9
aymenbouhaha <92325496+aymenbouhaha@users.noreply.github.com>
2023-04-29 02:18:05 +0100

    second commit

---
+++ android/app/src/main/kotlin/com/example/double_dating_front/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.double_dating_front
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/shared/network/remote/dio_helper.dart
@@ -0,0 +1,82 @@
+import 'package:dio/dio.dart';
+
+import '../../constants/constants.dart';
+
+class DioHelper{
+  static late Dio dio;
+  static void init(){
+    dio=Dio(
+        BaseOptions(
+            baseUrl:baseUrl,
+            receiveDataWhenStatusError: true
+        )
+    );
+  }
+
+  static Future getData(
+      {required String url,
+        Map<String,dynamic>? query,
+        String lang ='en',
+        dynamic token
+      }
+      )async{
+    dio.options.headers= {
+      'Content-Type' : 'application/json',
+      'lang': lang,
+      'Authorization': token??''
+    };
+    return await dio.get(url,queryParameters: query);
+  }
+
+  static Future postData(
+      {
+        required String url ,
+        required dynamic data,
+        dynamic query,
+        String lang ='en',
+        dynamic token
+      }
+      )async{
+    dio.options.headers= {
+      'Content-Type' : 'application/json',
+      'lang': lang,
+      'Authorization': token??''
+    };
+    return await dio.post(url,data:data,queryParameters: query );
+  }
+
+
+  static Future putData(
+      {
+        required String url ,
+        required dynamic data,
+        dynamic query,
+        String lang ='en',
+        dynamic token
+      }
+      )async{
+    dio.options.headers= {
+      'Content-Type' : 'application/json',
+      'lang': lang,
+      'Authorization': token??''
+    };
+    return await dio.put(url,data:data,queryParameters: query );
+  }
+
+  static Future deleteData(
+      {
+        required String url ,
+        dynamic query,
+        String lang ='en',
+        dynamic token
+      }
+      )async{
+    dio.options.headers= {
+      'Content-Type' : 'application/json',
+      'lang': lang,
+      'Authorization': token??''
+    };
+    return await dio.delete(url,queryParameters: query );
+  }
+
+}
